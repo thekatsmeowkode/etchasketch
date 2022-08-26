0fc036f748b981584f0489b45fbe2e119435dde6
Kat M <kmunson13@gmail.com>
2022-08-26 12:49:31 -0500

    Mouseover effect functional

--- etchasketch.js
+++ etchasketch.js
@@ -1,17 +1,21 @@
 let container = document.querySelector('.container')
-let changer = document.getElementsByClassName('.column')
+let changer = document.querySelectorAll('.column')
 
 let number = 0
 
 function makeGrid(number) {
-    for (let i = 0; i<number; i++)
-        {let row = document.createElement('div');
+    for (let i = 0; i < number; i++) {
+        let row = document.createElement('div');
         row.classList.add('row');
         container.appendChild(row);
-            for (let j=0; j<number; j++)
-            {let column = document.createElement('div')
+        for (let j = 0; j < number; j++) {
+            let column = document.createElement('div')
             column.classList.add('column')
-            row.appendChild(column)}}
+            row.appendChild(column)
+        }
+    }
 }
 
-makeGrid(50);
+makeGrid(30);
+
+container.addEventListener('mouseover', (event) => {event.target.style.backgroundColor = 'blue'})
