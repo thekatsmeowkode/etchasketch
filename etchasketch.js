bbd58ee2a9c93ca7e22675a00c8d2882f8930044
Kat M <kmunson13@gmail.com>
2022-08-29 08:56:40 -0500

    Add input field, code has bug to fix

--- etchasketch.js
+++ etchasketch.js
@@ -1,9 +1,30 @@
 let container = document.querySelector('.container')
 let changer = document.querySelectorAll('.column')
+let buttonPress = document.querySelector('.clearButton')
+let inputNumber = document.getElementById('inputNumber')
+let userSubmit = document.getElementById('submit')
 
-let number = 0
+userSubmit.addEventListener('click', makeGrid)
+buttonPress.addEventListener('click', eraseGrid)
 
-function makeGrid(number) {
+makeGrid()
+pencil()
+
+function makeGrid() {
+    let number = inputNumber.value
+    if (number == 0 || number > 101 || isNaN(number)) {
+        for (let i = 0; i < 20; i++) {
+            let row = document.createElement('div');
+            row.classList.add('row');
+            container.appendChild(row);
+            for (let j = 0; j < 20; j++) {
+                let column = document.createElement('div')
+                column.classList.add('column')
+                row.appendChild(column)
+            }
+        }
+    }
+    else if(number >= 1 || number <= 100) {
     for (let i = 0; i < number; i++) {
         let row = document.createElement('div');
         row.classList.add('row');
@@ -14,8 +35,19 @@ function makeGrid(number) {
             row.appendChild(column)
         }
     }
+}}
+
+pencil();
+
+function pencil(){container.addEventListener(
+    'mouseover', (event) => {event.target.style.backgroundColor = 'blue'})}
+
+
+function eraseGrid() {
+    let changer = document.querySelectorAll('.column');
+    for (let i=0; i < changer.length; i++)
+    {changer[i].style.backgroundColor = ''}
 }
 
-makeGrid(30);
 
-container.addEventListener('mouseover', (event) => {event.target.style.backgroundColor = 'blue'})
+
